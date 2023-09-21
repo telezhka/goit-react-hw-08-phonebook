3ae89eeafd69cc7535250af2128ceb3a1d6a7be4
Roman Telik <rtelik58@gmail.com>
2023-09-21 15:55:10 +0300

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -36,10 +36,10 @@ import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout'; //
-import { PrivateRoute } from './PrivateRoute';
-import { RestrictedRoute } from './RestrictedRoute';
-import { refreshUser } from 'redux/auth/operations';
-import { useAuth } from 'hooks';
+import { PrivateRoute } from './PrivateRoute'; //
+import { RestrictedRoute } from './RestrictedRoute'; //
+import { refreshUser } from 'redux/auth/operations'; //
+import { useAuth } from 'hooks'; //
 
 const HomePage = lazy(() => import('../pages/Home'));
 const RegisterPage = lazy(() => import('../pages/Register'));
