0ce60c31b0a7b4805e171bd4324d12dc0a7fdd26
Roman Telik <rtelik58@gmail.com>
2023-09-21 14:17:31 +0300

    smth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,87 @@
-import React, { useEffect } from 'react';
-import { ContactList } from './ContactsList';
-import { Form } from './Form';
-import { FilterInput } from './FilterInput';
-import { fetchContacts } from 'redux/operations';
+// import React, { useEffect } from 'react';
+// import { ContactList } from './ContactsList';
+// import { Form } from './Form';
+// import { FilterInput } from './FilterInput';
+// import { fetchContacts } from 'redux/operations';
+// import { useDispatch } from 'react-redux';
+// export const App = () => {
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     dispatch(fetchContacts());
+//   }, [dispatch]);
+
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       <div>
+//         <h1>Phonebook</h1>
+//         <Form />
+//         <h2>Contacts</h2>
+//         <FilterInput />
+//         <ContactList />
+//       </div>
+//     </div>
+//   );
+// };
+import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+import { refreshUser } from 'redux/auth/operations';
+import { useAuth } from 'hooks';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const RegisterPage = lazy(() => import('../pages/Register'));
+const LoginPage = lazy(() => import('../pages/Login'));
+const ContactsPage = lazy(() => import('../pages/Contacts'));
+
 export const App = () => {
   const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <div>
-        <h1>Phonebook</h1>
-        <Form />
-        <h2>Contacts</h2>
-        <FilterInput />
-        <ContactList />
-      </div>
-    </div>
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
