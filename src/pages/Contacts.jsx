f8cb4f265b5d5bde9f659cc1fdc3b6718118b534
Roman Telik <rtelik58@gmail.com>
2023-09-21 18:29:43 +0300

    another-try

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,13 @@
+export default function Tasks() {
+  return (
+    <>
+      {/* <Helmet>
+        <title>Your tasks</title>
+      </Helmet>
+      <TaskEditor />
+      <div>{isLoading && 'Request in progress...'}</div>
+      <TaskList /> */}
+      <h3>contacts!</h3>
+    </>
+  );
+}
