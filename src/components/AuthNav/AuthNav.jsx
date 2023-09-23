9571758d39513d9c02a1cc3a3544bc711690c7f3
Roman Telik <rtelik58@gmail.com>
2023-09-23 19:29:35 +0300

    styles

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,19 +1,19 @@
 import { NavLink } from 'react-router-dom';
-// import css from './AuthNav.module.css';
+import css from './AuthNav.module.css';
 
 export const AuthNav = () => {
   return (
-    // <div>
-    //   <NavLink className={css.link} to="/register">
-    //     Register
-    //   </NavLink>
-    //   <NavLink className={css.link} to="/login">
-    //     Log In
-    //   </NavLink>
-    //   </div>
-    <div>
-      <NavLink to="/register">Register</NavLink>
-      <NavLink to="/login">Log In</NavLink>
+    <div className={css.wrapper}>
+      <NavLink className={css.link} to="/register">
+        Register
+      </NavLink>
+      <NavLink className={css.link} to="/login">
+        Log In
+      </NavLink>
     </div>
+    // <div>
+    //   <NavLink to="/register">Register</NavLink>
+    //   <NavLink to="/login">Log In</NavLink>
+    // </div>
   );
 };
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,24 +1,20 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks';
-// import css from './Navigation.module.css';
+import css from './Navigation.module.css';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    // <nav>
-    //   <NavLink className={css.link} to="/">
-    //     Home
-    //   </NavLink>
-    //   {isLoggedIn && (
-    //     <NavLink className={css.link} to="/tasks">
-    //       Tasks
-    //     </NavLink>
-    //   )}
-    // </nav>
     <nav>
-      <NavLink to="/">Home</NavLink>
-      {isLoggedIn && <NavLink to="/tasks">Tasks</NavLink>}
+      <NavLink to="/" className={css.link}>
+        Home
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink to="/contacts" className={css.link}>
+          Contacts
+        </NavLink>
+      )}
     </nav>
   );
 };
